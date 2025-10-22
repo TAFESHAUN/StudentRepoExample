d8a83bf7ffd732e57220477eff45868bfca49ece
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-10-22 11:19:36 +1100

    Added CSV + IRepo and Student data

---
+++ StudentRepoExample/DataModel/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentRepoExample.DataModel
+{
+    //Data Model for student records > REPO (CSV OR DB SQL)
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Age { get; set; }    
+        public string YearGroup { get; set; } = "";
+        public string SportsTeam { get; set; } = "";
+    }
+}
---
+++ StudentRepoExample/IRepo.cs
@@ -0,0 +1,24 @@
+﻿using StudentRepoExample.DataModel;
+
+namespace StudentRepoExample
+{
+    //Defualt Repo for student records
+    public interface IRepo
+    {
+        //APPLY this called CRUD operations
+
+        //1.READ in a list of students
+        List<Student> GetStudents(); // #TODO Make it more flex by getting generic records instead
+
+        //2.CREATE a new student record
+        void AddStudent(Student addStudent);
+
+        //3.UPDATE an existing student record
+        void UpdateStudent(Student upStudent);
+
+        //4.DELETE a student record
+        void DeleteSelectedStudent(int delStudent); // #TODO Pass obj or ID?
+
+
+    }
+}
